446de17e1615c440bd4caece8da33c4383fb2826
ait larbi ben hachmi oumaima <aitlarbibenhachmioumaima@gmail.com>
2026-04-29 08:52:02 +0100

    first commit

---
+++ production-epierrage/src/main/java/com/ocp/production_epierrage/ProductionEpierrageApplication.java
@@ -0,0 +1,13 @@
+package com.ocp.production_epierrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionEpierrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionEpierrageApplication.class, args);
+	}
+
+}
---
+++ production-epierrage/src/test/java/com/ocp/production_epierrage/ProductionEpierrageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp.production_epierrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionEpierrageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
