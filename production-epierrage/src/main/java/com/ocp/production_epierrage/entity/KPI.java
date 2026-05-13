2b8fb18c57060c45dbd03c19adb5952299bef1d6
ait larbi ben hachmi oumaima <aitlarbibenhachmioumaima@gmail.com>
2026-05-13 08:19:16 +0100

    add

--- production-epierrage/src/main/java/com/ocp/production_epierrage/entity/KPI.java
+++ production-epierrage/src/main/java/com/ocp/production_epierrage/entity/KPI.java
@@ -1,11 +1,10 @@
 package com.ocp.production_epierrage.entity;
 
 
+
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "kpis")
@@ -16,17 +15,84 @@ public class KPI {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    @Column(name = "date_locale")
-    private LocalDate dateLocale;
-
-    private Integer semaine;
+    @Column(nullable = false)
+    private LocalDate date;
 
 
 
+    @Column(name = "hm_ep")
+    private Double hmEp;
+    @Column(name = "rdt_p")
+    private Double rdtP;
+
+    @Column(name = "rdt_r")
+    private Double rdtR;
+
+
+    @Column(name = "td_mecanique")
+    private Double tdMecanique;
+    @Column(name = "tu_utilisation")
+    private Double tu;
+
+    @Column(name = "td_exploitation")
+    private Double tdExploitation;
+
+
+
+    private Double arretMecanique;
+    private Double arretElectrique;
+    private Double arretElectronique;
+    private Double arretExploitation;
+    private Double arretMateriel;
+    private Double arretDecide;
+    private Double arretKoch;
+    private Double arretPlanifie;
+    private Double arretExterieur;
+    private Double arretAttenteCamion;
+
+
+    @Column(name = "a_exp")
+    private Double aExp;
+    @Column(name = "a_ext")
+    private Double aExt;
+    @Column(name = "a_mat")
+    private Double aMat;
+    @Column(name = "t_dis")
+    private Double tDis;
+    @Column(name = "taux_utilisation_detail")
+    private Double tauxUtilisation;
+
+
+
+    private Double tonnage;
+
+    @Column(name = "the")
+    private Double tonnageHumideEpierre;
+
+    @Column(name = "thc")
+    private Double tonnageHumideCrible;
+    @Column(name = "hm_marche")
+    private Double heuresDeMarche;
+
+    private Double mtbf;
+    private Double debit;
+
+    @Column(name = "nbre_pannes")
+    private Integer nbrePannes;
+
+    private Double oee;
+
+
+
+    private String semaine;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "utilisateur_id")
     private Utilisateur utilisateur;
 }
+
+
+
+
+
